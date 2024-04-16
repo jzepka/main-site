26ce763c4a55104e1d88fb43bf678eee4dbf30ae
jzepka <161222490+jzepka@users.noreply.github.com>
2024-04-16 09:58:37 +0200

    Update works-img.js

--- js/works-img.js
+++ js/works-img.js
@@ -1,4 +1,4 @@
-var images = ["img/boilerroom/home/1.JPG", "img/boilerroom/home/2.jpg", "img/boilerroom/home/4.jpg", "img/boilerroom/home/5.jpg", "img/boilerroom/home/6.jpg", "img/boilerroom/home/7.jpg", "img/boilerroom/home/10.jpg", "img/boilerroom/home/11.jpg", "img/boilerroom/home/16.jpg", "img/boilerroom/home/17.jpg", "img/boilerroom/home/18.jpg",
+var images = ["img/boilerroom/home/1.jpg", "img/boilerroom/home/2.jpg", "img/boilerroom/home/4.jpg", "img/boilerroom/home/5.jpg", "img/boilerroom/home/6.jpg", "img/boilerroom/home/7.jpg", "img/boilerroom/home/10.jpg", "img/boilerroom/home/11.jpg", "img/boilerroom/home/16.jpg", "img/boilerroom/home/17.jpg", "img/boilerroom/home/18.jpg",
 "img/boilerroom/home/22.jpg", "img/boilerroom/home/23.jpg", "img/boilerroom/home/24.jpg"];
 
 var imgState = 0;
